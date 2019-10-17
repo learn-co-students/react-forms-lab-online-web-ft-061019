d2ea5e96027131927ecee5a2d99b8b149ee152a3
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-17 11:38:45 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,57 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+      //valid: false
+    };
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target
+    
+    // if (this.state.username !== "" && this.state.password !== "") {
+    //   this.setState({
+    //     valid: true
+    //   })
+    // } else {
+    //   this.setState({
+    //     valid: false
+    //   })
+    // }
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.username !== "" && this.state.password !== "") {
+      this.props.handleLogin(this.state)
+    }
+  }
+
+  invalidLogin = event => {
+    debugger
+    event.preventDefault()
+    //console.log("Username and Password must both be filled out")
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" onChange={this.handleInputChange} name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" onChange={this.handleInputChange} name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
